e698bddf27dbef787cc15360d006fcc6a4d1fd65
BohanWu <819186192@qq.com>
2024-02-15 21:07:55 -0600

    Orphanxxx.py

--- PythonTemplate/OrphanPagesMapper.py
+++ PythonTemplate/OrphanPagesMapper.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 import sys
 
-
 for line in sys.stdin:
-  # TODO
-  
-  # print('%s\t%s' % (  ,  )) pass this output to reducer
+    # Split the line into page ID and linked pages
+    page_id, links = line.strip().split(':')
+    linked_page_ids = links.split()
+
+    # Emit the page ID with a marker to indicate it has links
+    print(f'{page_id}\t{"P"}')
+
+    # Emit the linked page IDs with a marker to indicate they are linked to
+    for linked_page_id in linked_page_ids:
+        if linked_page_id != page_id:
+            print(f'{linked_page_id}\t{"L"}')
--- PythonTemplate/OrphanPagesReducer.py
+++ PythonTemplate/OrphanPagesReducer.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 import sys
 
-
-#TODO
-
+linked_pages = set()
+all_pages = set()
 
 for line in sys.stdin:
-  # TODO
+    # Parse the input from mapper
+    page_id, page_type = line.strip().split('\t')
 
-#TODO
-# print(xx) print as final output
+    if page_id not in all_pages:
+        all_pages.add(page_id)
+
+    # If the page is linked to by another, add to linked_pages set
+    if page_type == 'L':
+        if page_id not in linked_pages:
+            linked_pages.add(page_id)
+
+# Orphan pages are those that are not in linked_pages set but in parent_pages
+orphan_pages = all_pages - linked_pages
+
+# Print orphan pages sorted in increasing order
+for orphan_page in sorted(orphan_pages, key=int):
+    print(orphan_page)
